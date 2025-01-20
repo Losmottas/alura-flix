5ae454b2227ec66be476b5c089a3c762060a338d
zagu5 <zury.agudelo@gmail.com>
2025-01-20 17:16:37 -0500

    contruccion estructura proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>¡BIenvenido a Alura Fix!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
